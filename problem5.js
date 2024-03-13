b1b005568f705af84b11574e96ef8a53a48c022d
imran122 <imransardar122@gmail.com>
2024-03-13 13:19:52 +0600

    problem5

--- problem5.js
+++ problem5.js
@@ -2,11 +2,11 @@ const nums = [6, 1, 2, 4, 3, 4, 5, 6];
 const duplicates = [];
 
 for (let i = 0; i < nums.length; i++) {
-  const index = Math.abs(nums[i]) - 1;
+  const index = nums[i];
   console.log(index);
   // If the value at index is negative, it means it has already been seen
   if (nums[index] < 0) {
-    duplicates.push(index + 1);
+    duplicates.push(index);
   } else {
     nums[index] = -nums[index];
   }
